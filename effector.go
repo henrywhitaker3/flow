ed56252d0a5e86dd6ebad315a246a0921a3efea3
Henry Whitaker <henrywhitaker3@outlook.com>
2024-06-18 12:42:58 +0100

    feat: added throttle in method

--- effector.go
+++ effector.go
@@ -3,3 +3,4 @@ package flow
 import "context"
 
 type Effector[T any] func(context.Context) (T, error)
+type EffectorIn[T any] func(context.Context, T) error
--- throttle.go
+++ throttle.go
@@ -11,6 +11,46 @@ var (
 	ErrThrottled = errors.New("throttled")
 )
 
+func ThrottleIn[T any](f EffectorIn[T], every time.Duration) EffectorIn[T] {
+	called := false
+	once := &sync.Once{}
+
+	return func(ctx context.Context, t T) error {
+		once.Do(func() {
+			ticker := time.NewTicker(every)
+			go func() {
+				defer ticker.Stop()
+				for {
+					select {
+					case <-ctx.Done():
+						return
+					case <-ticker.C:
+						called = false
+					}
+				}
+			}()
+		})
+
+		if !called {
+			called = true
+			return f(ctx, t)
+		}
+
+		return ErrThrottled
+	}
+}
+
+func ThrottleInSilently[T any](f EffectorIn[T], every time.Duration) EffectorIn[T] {
+	tf := ThrottleIn[T](f, every)
+	return func(ctx context.Context, t T) error {
+		err := tf(ctx, t)
+		if errors.Is(err, ErrThrottled) {
+			return nil
+		}
+		return err
+	}
+}
+
 // Return an Effector that, when called, will only fire once every duration
 func Throttle[T any](f Effector[T], every time.Duration) Effector[T] {
 	called := false
