855906306bc8f36a35e632dda9ae8f9713522855
Henry Whitaker <henrywhitaker3@outlook.com>
2024-08-08 07:19:07 +0100

    feat: added once per struct

---
+++ once.go
@@ -0,0 +1,23 @@
+package flow
+
+import "sync"
+
+type OncePer[T comparable] struct {
+	onces map[T]*sync.Once
+}
+
+func NewOncePer[T comparable]() *OncePer[T] {
+	return &OncePer[T]{
+		onces: make(map[T]*sync.Once),
+	}
+}
+
+func (o *OncePer[T]) Do(key T, f func()) {
+	once, ok := o.onces[key]
+	if !ok {
+		once = &sync.Once{}
+		o.onces[key] = once
+	}
+
+	once.Do(f)
+}
---
+++ once_test.go
@@ -0,0 +1,25 @@
+package flow
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestOncePerSetsUpNewOnceIfDoesntExist(t *testing.T) {
+	once := NewOncePer[string]()
+	called := 0
+	once.Do("bongo", func() {
+		called++
+	})
+	once.Do("bongo", func() {
+		called++
+	})
+	once.Do("bingo", func() {
+		called++
+	})
+	once.Do("bingo", func() {
+		called++
+	})
+	require.Equal(t, 2, called)
+}
